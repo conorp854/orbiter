9200eed02a49e75d35006d6611edbf7bb8cb13cc
conorp854 <conor.de-paor@ucdconnect.ie>
2021-01-19 20:55:19 +0000

    Initial notes

---
+++ source/path.py
@@ -0,0 +1,22 @@
+#Make an animation of the earth orbiting the sun
+#Conor Power
+
+# Not considering angular momentum of planets yet
+
+# F = (G * m1 * m2)/d ** 2
+#  F: force between two objects with mass
+#  G: Gravitational Constant
+# m1: mass of object 1
+# m2: mass of object 2
+#  d: distance between the centres of mass of the two objects
+
+# Mass of Sun:    1.989 * 10 **  30 [kg]
+# Mass of Earth:  5.972 * 10 **  24 [kg]
+# G:              6.673 * 10 ** -20 [km**3]/[kg s**2] 
+
+
+#Reference:
+# https://medium.com/swlh/create-your-own-n-body-simulation-with-python-f417234885e9
+# Fundamentals of Astrodynamics; Bate
+# https://en.wikipedia.org/wiki/Leapfrog_integration
+
